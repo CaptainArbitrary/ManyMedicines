870bd7883e1f669b53ad078e2ed8309db968216f
Jeffery Harrell <jefferyharrell@gmail.com>
2023-07-25 18:53:38 -0700

    source code cleanup

--- Source/Assemblies/MMeds/HediffComp_TreatToxicBuildup.cs
+++ Source/Assemblies/MMeds/HediffComp_TreatToxicBuildup.cs
@@ -1,26 +1,13 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-
-using RimWorld;
 using Verse;
 
 namespace MMeds
 {
-    
-    public class HediffCompProperties_TreatToxicBuildup : HediffCompProperties
-    {
-        public float severityPerDay;
-
-        public HediffCompProperties_TreatToxicBuildup()
-        {
-            compClass = typeof(HediffComp_TreatToxicBuildup);
-        }
-    }
-
     public class HediffComp_TreatToxicBuildup : HediffComp
     {
         public HediffCompProperties_TreatToxicBuildup Props => (HediffCompProperties_TreatToxicBuildup)props;
